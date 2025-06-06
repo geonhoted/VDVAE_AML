9ccba8b1370740ec05da99afd664d7ed053e9abd
SongWon03 <146622123+SongWon03@users.noreply.github.com>
2025-06-06 18:01:03 +0900

    Update setup_cifar10.sh

--- setup_cifar10.sh
+++ setup_cifar10.sh
@@ -1,2 +1,6 @@
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xf cifar-10-python.tar.gz
+
+# if toronto server is closed, use under.
+wget https://data.brainchip.com/dataset-mirror/cifar10/cifar-10-python.tar.gz
+tar -xf cifar-10-python.tar.gz
